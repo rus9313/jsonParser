dacaabfcba8ffd32727320331fadd9a2072f10ea
Руслан <ruslanzhuminov@mail.ru>
2022-02-07 20:13:56 +0300

    второй коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        Car car = new Car("yellow", "renault");
-        //objectMapper.writeValue(new File("target/car.json"), car);
+        //  Car car = new Car("yellow", "renault");
+        //  //objectMapper.writeValue(new File("target/car.json"), car);
 
-        System.out.println(objectMapper.writeValueAsString(car));
+        //  System.out.println(objectMapper.writeValueAsString(car));
+
+        String json = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        Car car = objectMapper.readValue(json, Car.class);
+
+        System.out.println(car);
     }
 
 }
